18d9f7ecbb5fbf5e9bc34c5fe173dbbafb68dea4
Knoisy27 <joseluay1999@gmail.com>
2025-10-05 21:43:20 -0500

    Elimina archivos de configuración de prerenderizado y actualiza las rutas del servidor para utilizar el modo de prerenderizado en las rutas principales.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,35 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'destinos',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'viajes-personalizados',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'sobre-nosotros',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contacto',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'terminos',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'privacidad',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
