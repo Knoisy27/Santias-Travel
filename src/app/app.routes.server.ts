a0e775337a11682cf1b572deb11b2dfe4655b911
Knoisy27 <joseluay1999@gmail.com>
2026-01-18 16:49:24 -0500

    Update Angular CLI and various Algolia packages to version 20.2.3 and 5.35.0 respectively, ensuring compatibility and performance improvements. Adjust server routing configuration to use Client render mode for better handling of dynamic routes.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,35 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'viajes-grupales',
-    renderMode: RenderMode.Prerender
-  },
-      {
-        path: 'viajes-a-tu-medida',
-        renderMode: RenderMode.Prerender
-      },
-  {
-    path: 'sobre-nosotros',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'contacto',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'terminos',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'privacidad',
-    renderMode: RenderMode.Prerender
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   }
 ];
